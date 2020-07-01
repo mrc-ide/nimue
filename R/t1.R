391acebf790001ddf4008d122cd5abdd45db6b0b
Peter Winskill <p.winskill@imperial.ac.uk>
2020-07-01 15:18:59 +0100

    Test functionality

---
+++ R/t1.R
@@ -0,0 +1,10 @@
+#' T1
+#'
+#' @param a A
+#' @param b B
+#'
+#' @return sum
+#' @export
+t1 <- function(a, b){
+  a + b
+}
---
+++ tests/testthat/test-t1.R
@@ -0,0 +1,3 @@
+test_that("addition works", {
+  expect_equal(t1(1, 1), 2)
+})
